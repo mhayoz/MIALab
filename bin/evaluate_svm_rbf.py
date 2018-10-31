f57280cb0c017c8b0d48a8d0ec8a8d169a9263e7
mat0 <matthias.fontanellaz@gmail.com>
2018-10-31 18:19:08 +0100

    added new results

--- bin/evaluate_svm_rbf.py
+++ bin/evaluate_svm_rbf.py
@@ -60,11 +60,11 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
     print('-' * 5, 'Testing...')
 
     # load classifier
-    file_id = open('svm_rbf.pckl', 'rb')
+    file_id = open('svm_rbf_fullset.pckl', 'rb')
     svm_rbf_classifier = pickle.load(file_id)
     file_id.close()
 
-    file_id = open('scaler_rbf.pckl', 'rb')
+    file_id = open('scaler_rbf_fullset.pckl', 'rb')
     scaler = pickle.load(file_id)
     file_id.close()
 
@@ -116,7 +116,7 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
     #                       img.id_ + '-PP')
 
         # save results
-        sitk.WriteImage(images_prediction[i], os.path.join(result_dir, images_test[i].id_ + '_SEG.mha'), True)
+        sitk.WriteImage(images_prediction[i], os.path.join(result_dir, images_test[i].id_ + '_SEG_SVM_fullset.mha'), True)
      #   sitk.WriteImage(images_post_processed[i], os.path.join(result_dir, images_test[i].id_ + '_SEG-PP.mha'), True)
 
 
